edd2d6db9bdc82f03ed1f13b93b31666e0802482
FMartineezz <facmartinez@frba.utn.edu.ar>
2025-09-09 22:21:21 -0300

    Integracion con postgres

--- src/main/java/ar/edu/utn/dds/k3003/repository/JpaPdiRepository.java
+++ src/main/java/ar/edu/utn/dds/k3003/repository/JpaPdiRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Profile("Deploy")
+@Profile("deploy")
 public interface JpaPdiRepository extends JpaRepository<PdI, Long>, PdIRepository {
     List<PdI> findByHechoId(String hechoId);
 }
